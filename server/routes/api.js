777a9842480a8b00ff0394a9433de66385bb41a3
distilledx <alissxlace@proton.me>
2022-11-03 16:04:19 +0530

    changing api

--- server/routes/api.js
+++ server/routes/api.js
@@ -58,7 +58,7 @@ router.post('/upload', async (req, res) => {
     let users = JSON.parse(fs.readFileSync('./server/users.json'));
 
     let userId = req.body.session;
-    let fileId = req.body.fileId;
+    let fileId = req.body.fileId; // This is the file id from the client
 
     let user = 'Anon';
 
